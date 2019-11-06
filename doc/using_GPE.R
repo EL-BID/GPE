eb62a1440ded57212d60fa341dab7028ef7557cb
HAVB <avazquez@gmail.com>
2019-11-05 22:22:33 -0300

    cleaning up the documentation

--- doc/using_GPE.R
+++ doc/using_GPE.R
@@ -2,17 +2,14 @@
 knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE)
 
 ## ------------------------------------------------------------------------
-participants <- read.csv("../data/participants.csv")
+library(GPE)
 
+## ------------------------------------------------------------------------
 head(participants)
 
 ## ------------------------------------------------------------------------
-locations <- read.csv("../data/locations.csv")
-
 head(locations)
 
 ## ------------------------------------------------------------------------
-library(GPE)
-
 GPE_plot_map(participants, locations)
 
